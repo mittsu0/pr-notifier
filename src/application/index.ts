5dc8942e30b6eb0513a0f3b93dc85e46896943ef
mittsu0 <threebridge3824@gmail.com>
2025-12-14 22:35:08 +0900

    feat: application層のexport

---
+++ src/application/index.ts
@@ -0,0 +1,5 @@
+export { NotifyPullRequestsUseCase } from './NotifyPullRequestsUseCase.js';
+export type {
+  NotifyPullRequestsInput,
+  NotifyPullRequestsOutput,
+} from './NotifyPullRequestsUseCase.js';
